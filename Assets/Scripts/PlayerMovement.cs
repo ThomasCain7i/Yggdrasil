e6cee259a10b40b2706d4ae22564184e5ee13a10
unknown <Thomas@LON-C7-7968.students.local>
2023-02-03 19:09:23 +0000

    Initial Commit
    The start of something great.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController ChrController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float Gravity = 20.0f;
+
+    Vector3 moveDirection = Vector3.zero;
+
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChrController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ChrController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        else
+        {
+            moveDirection.x = Input.GetAxis("Horizontal") * speed;
+        }
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            anim.SetBool("isSlowRunning", true);
+
+            float xScale = Mathf.Abs(transform.localScale.x);
+
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                transform.localScale = new Vector3(-xScale, transform.localScale.y, transform.localScale.z);
+            }
+            else
+            {
+                transform.localScale = new Vector3(xScale, transform.localScale.y, transform.localScale.z);
+
+            }
+        }
+        else
+        {
+            anim.SetBool("isSlowRunning", false);
+        }
+
+        moveDirection.y -= Gravity * Time.deltaTime;
+
+        ChrController.Move(moveDirection * Time.deltaTime);
+    }
+}
