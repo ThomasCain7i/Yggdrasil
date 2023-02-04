254e574446c2e8eee8caad3d546f1c4420fa144e
m-hayes95 <michaelhayes322@gmail.com>
2023-02-04 11:30:44 +0000

    Added enemyAI movement using distance vector 3. Changed constraints on player (Mike)
    Added enemyAI movement using distance vector 3. Changed constraints on player on rigidbody z axis, to stop pushing off the map (Mike).

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,25 +4,46 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    //ref to player character
     public GameObject player;
+    //ref to patrol points
+    public GameObject pointA, pointB;
+    //how much damage enemy does to player
     public float enemyDamage;
-    [SerializeField] private float enemySpeed;
-    
-    
+    public float playerDistance = 5f;
+    [SerializeField] private float enemySpeed = 1f;
+    //check which way enemy is facing
+    public enum EnemyFacing { f, b }
+    public EnemyFacing enemyDirection;
+    //enemy states
+    public enum EnemyAiSM { idle, chasing }
+    public EnemyAiSM enemyAiSM;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //On game run, look for instiated player actor
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
         //Distance checker, to chase player
+        Debug.Log("Enemy is " + Vector3.Distance(player.transform.position, transform.position) + "from player");
 
         //if distance is less than... chase player
-
-        //patrol
+        if (Vector3.Distance(player.transform.position, transform.position) < playerDistance)
+        {
+            //chase player
+            Debug.Log("Chase player");
+            transform.LookAt(player.transform.position);
+            transform.Translate(Vector3.forward * enemySpeed * Time.deltaTime);
+        }
+        else
+        {
+            //patrol
+        }
 
 
     }
