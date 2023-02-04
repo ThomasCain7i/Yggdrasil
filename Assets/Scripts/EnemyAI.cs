d570d982b491c35021d9664d4e9627a59fc9da0d
m-hayes95 <michaelhayes322@gmail.com>
2023-02-04 11:06:02 +0000

    Added enemy AI Script and changed Prefabs in Mike's Scene (Mike)
    Added enemy AI Script and changed Prefabs in Mike's Scene (Mike).

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    public GameObject player;
+    public float enemyDamage;
+    [SerializeField] private float enemySpeed;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,12 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Distance checker, to chase player
+
+        //if distance is less than... chase player
+
+        //patrol
+
+
     }
 }
