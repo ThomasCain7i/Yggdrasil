8bb64cc040cf6a11f63ee0e5dd61538990053b20
unknown <Thomas@LON-C7-7968.students.local>
2023-02-05 11:03:40 +0000

    Text Demo Text (Thomas)

--- Assets/Scripts/Text/DialogueManager.cs
+++ Assets/Scripts/Text/DialogueManager.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
+    public TextMeshProUGUI nametext;
+    public TextMeshProUGUI dialogueText;
+
+    public Animator animator;
+
     private Queue<string> sentences;
 
     // Start is called before the first frame update
@@ -14,6 +21,32 @@ public class DialogueManager : MonoBehaviour
     
     public void StartDialogue(Dialogue dialogue)
     {
-        Debug.Log("Starting conversation with " + dialogue.name);
+        animator.SetBool("isOpen", true);
+
+        nametext.text = dialogue.name;
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    void EndDialogue()
+    {
+        animator.SetBool("isOpen", false);
     }
 }
